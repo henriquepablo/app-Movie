949bda492a715da375163dc32ba9c05b561c1514
henriquepablo <pablo269704@gmail.com>
2024-07-13 08:22:39 -0300

    feat: listing top rated films in the screen

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Box, FlatList, Heading} from "@gluestack-ui/themed";
 import api from "../services/api";
 import ListPopularMovies from "../components/ListPopularMovies";
+import ListTopRatedFilms from "../components/ListTopRatedFilms";
+import { ScrollView } from "react-native";
 
 function Home():JSX.Element {
 
     const [listFilmesPopular, setListFilmesPopular] = useState([]);
+    const [listFilmsRated, setListFilmsRated] = useState([]);
 
     async function loadFilmsPopular() {
         const response = await api.get('discover/movie?include_adult=false&include_video=false&language=pt-BR&page=1&sort_by=popularity.desc')
@@ -15,11 +18,22 @@ function Home():JSX.Element {
         .catch(err => console.log(err));
     }
 
+    async function loadFilmsRated() {
+        const response = await api.get('discover/movie?include_adult=false&include_video=false&language=pt-BR&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200')
+        .then(response => {
+            setListFilmsRated(response.data.results)
+        })
+        .catch(err => console.log(err))
+        
+    }
+
     useEffect(() => {
         loadFilmsPopular();
-    }, [listFilmesPopular]); 
+        loadFilmsRated();
+    }, []); 
 
     return(
+        <ScrollView style={{flex: 1, backgroundColor: '#0F111D'}}>
         <Box flex={1} bg="#0F111D">
             <Box mt={50} ml={20} mb={20}>
                 <Heading color="#fff" size="xl" fontFamily="$heading">
@@ -33,10 +47,28 @@ function Home():JSX.Element {
                 renderItem={({item}:any) => <ListPopularMovies pathImage={item.poster_path} id={item.id} />}
                 showsHorizontalScrollIndicator={false}
                 horizontal={true}
-                
+                maxHeight={250}
+                mr={20}
+            />
+
+            <Box mt={30} ml={20} mb={20}>
+                <Heading color="#fff" size="xl" fontFamily="$heading">
+                    Mais Votados
+                </Heading>
+            </Box>
+
+            <FlatList 
+                data={listFilmsRated}
+                keyExtractor={(item:any) => item.id}
+                renderItem={({item}:any) => <ListTopRatedFilms pathImage={item.poster_path} name={item.title} vote_average={item.vote_average} id={item.id}/>}
+                showsHorizontalScrollIndicator={false}
+                horizontal={true}
+                maxHeight={370}
+                mr={20}
             />
             
         </Box>
+        </ScrollView>
     );
 }
 
---
+++ src/components/ListTopRatedFilms.tsx
@@ -0,0 +1,40 @@
+import { Box, Icon, Image, Text, StarIcon, HStack } from "@gluestack-ui/themed";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+
+type props = {
+    pathImage: string,
+    name: string
+    vote_average: string,
+    id: string
+}
+
+function ListTopRatedFilms({pathImage, name, vote_average, id}:props):JSX.Element {
+    return(
+        <Box borderRadius={10} backgroundColor="#292B37" width={200} height={370} ml={20} >
+            
+            <TouchableOpacity activeOpacity={0.5} >
+            
+                <Image source={{uri: `https://image.tmdb.org/t/p/original${pathImage}`}} alt="image films" height={260} width={200} borderRadius={10} borderBottomLeftRadius={0} borderBottomRightRadius={0}/>
+
+                <Box padding={15} >
+            
+                    <Text color="#fff" bold fontSize={17} mb={15}>
+                        {name}
+                    </Text>
+            
+                    <HStack space="xs" alignItems="center">
+                        <Icon as={StarIcon} size="md" color="#FFE500"/>
+                        <Text color="#fff" >
+                            {vote_average}
+                        </Text>
+                    </HStack>
+                </Box>
+            
+            </TouchableOpacity>
+        
+        </Box>
+    );
+}
+
+export default ListTopRatedFilms
