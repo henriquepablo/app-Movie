0bc2080ff9fd4601eed426ad04431e7dad51c12b
henriquepablo <pablo269704@gmail.com>
2024-07-13 14:09:40 -0300

    "feat: updating state id film and navigation screen"

--- src/components/ListPopularMovies.tsx
+++ src/components/ListPopularMovies.tsx
@@ -1,6 +1,9 @@
 import { HStack, Image, Box} from "@gluestack-ui/themed";
 import React from "react";
 import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch} from "react-redux";
+import { id_film } from "../Redux/Reducers/idFilme";
 
 type props = {
     pathImage: string
@@ -8,13 +11,21 @@ type props = {
 }
 
 function ListPopularMovies({pathImage, id}:props):JSX.Element {
+
+    const navigation = useNavigation();
+
+    const dipatch = useDispatch();
+
     return(
         <Box borderRadius={10} backgroundColor="#00000087" width={160} height={250} ml={20} >
             
-            <TouchableOpacity activeOpacity={0.5} >
+            <TouchableOpacity activeOpacity={0.5} onPress={ () => {
+                dipatch(id_film(id));
+                navigation.navigate('PageSelected' as never);
+            }}>
             
                 <Image source={{uri: `https://image.tmdb.org/t/p/original${pathImage}`}} alt="image films" height={250} width={160} borderRadius={10}/>
-            
+                
             </TouchableOpacity>
         
         </Box>
