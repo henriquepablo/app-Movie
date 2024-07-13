378f564e686e97e416849fda080758e4e1e6e9b1
henriquepablo <pablo269704@gmail.com>
2024-07-13 20:11:31 -0300

    "feat: displaying more informations about top rateds films"

--- src/components/ListTopRatedFilms.tsx
+++ src/components/ListTopRatedFilms.tsx
@@ -1,6 +1,9 @@
 import { Box, Icon, Image, Text, StarIcon, HStack } from "@gluestack-ui/themed";
 import React from "react";
 import { TouchableOpacity } from "react-native";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import { id_film } from "../Redux/Reducers/idFilme";
 
 type props = {
     pathImage: string,
@@ -10,10 +13,18 @@ type props = {
 }
 
 function ListTopRatedFilms({pathImage, name, vote_average, id}:props):JSX.Element {
+
+    const navigation = useNavigation();
+
+    const dipatch = useDispatch();
+
     return(
         <Box borderRadius={10} backgroundColor="#292B37" width={200} height={390} ml={20} >
             
-            <TouchableOpacity activeOpacity={0.5} >
+            <TouchableOpacity activeOpacity={0.5} onPress={ () => {
+                dipatch(id_film(id));
+                navigation.navigate('PageSelected' as never);
+            }}>
             
                 <Image source={{uri: `https://image.tmdb.org/t/p/original${pathImage}`}} alt="image films" height={260} width={200} borderRadius={10} borderBottomLeftRadius={0} borderBottomRightRadius={0}/>
 
