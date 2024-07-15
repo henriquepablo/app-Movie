08cfd690b7ff1c803a9ea88b89f4752455824124
henriquepablo <pablo269704@gmail.com>
2024-07-15 07:08:00 -0300

    feat: creating component input

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,12 +1,31 @@
-import React from "react";
-import { Box, Text } from "@gluestack-ui/themed";
+import React, { useState } from "react";
+import { Box, Input, InputField, InputIcon, InputSlot, SearchIcon } from "@gluestack-ui/themed";
 
 function Search():JSX.Element {
+
+    const [nameFilm, setNameFilm] = useState('');
+
     return(
-        <Box>
-            <Text>
-                Screen Search
-            </Text>
+        <Box flex={1} backgroundColor="#0F111D">
+        
+            <Box mt={50} justifyContent="center" alignItems="center">
+                
+                <Input w="90%" borderRadius={10} h={55} backgroundColor="#fff" $focus-borderColor="#000">
+                    
+                    <InputSlot>
+                        <InputIcon as={SearchIcon} ml={10} size="md" color="#000"/>
+                    </InputSlot>
+                    
+                    <InputField 
+                        placeholder="Pesquisar filme ou série" color="#000" 
+                        placeholderTextColor="#000"
+                        onChangeText={(text) => setNameFilm(text)}
+                        fontSize={17}
+                        fontWeight="$medium"
+                    />
+                </Input>
+            </Box>
+            
         </Box>
     );
 }
